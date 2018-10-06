b11fb93ff16e79952d6e76a4a0b0cfda97192efc
Karthikeyan Annadorai <karthik83@gmail.com>
2018-10-06 15:47:28 -0500

    Handling user action to send message

--- src/App.jsx
+++ src/App.jsx
@@ -11,42 +11,59 @@ class App extends Component {
 	  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
 	  messages: [
 	    {
+	      id: "1",
 	      username: "Bob",
-	      content: "Has anyone seen my marbles?",
-	      id: "1"
+	      content: "Has anyone seen my marbles?"
 	    },
 	    {
+	      id: "2",
 	      username: "Anonymous",
-	      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-	      id: "2"
+	      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
 	    },
 	    {
+	      id: "3",
 	      username: "Sam",
-	      content: "I found it, Buddy!",
-	      id: "3"
+	      content: "I found it, Buddy!"
 	    }
 	  ]
 	};
   }
 
-componentDidMount() {
-  setTimeout(() => {
-    // Add a new message to the list of messages in the data store
-    const newMessage = {username: "Michelle", content: "No no no no, that is a different one!", id: "4"};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-  }, 3000);
-}
-
+	componentDidMount() {
+	  setTimeout(() => {
+	    // Add a new message to the list of messages in the data store
+	    const newMessage = {username: "Michelle", content: "No no no no, that is a different one!", id: "4"};
+	    const messages = this.state.messages.concat(newMessage);
+	    // Update the state of the app component.
+	    // Calling setState will trigger a call to render() in App and all child components.
+	    this.setState({messages: messages});
+	  }, 3000);
+	};
+  
+  // Update current user to the state
+  updateUser = username => {
+  	this.setState({currentUser:{name:username}});
+   };
+  
+  // Add new message to the state
+  addMessage = (content, username) => {
+  	const newMessage = {
+      id: this.state.messages.length + 1,
+      username: username,
+      content: content
+    };
+    const messages = this.state.messages.concat(newMessage);
+    this.setState({messages: messages});
+  };
 
   render() {
     return (
 	  <div>
 	    <NavBar />
         <MessageList messages={this.state.messages} />
-  		<ChatBar name={this.state.currentUser.name} />
+  		<ChatBar name={this.state.currentUser.name} 
+  			addMessage={this.addMessage} 
+  			updateUser={this.updateUser} />
   	  </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
 class ChatBar extends Component {
+
+// Enter key event handler
+handleMessageKeyPress = (event) => {
+  if(event.key == 'Enter'){
+  	let username = document.getElementById('username').value;
+  	if (username === null || username === "") {
+ 	  username = "Anonymous";
+  	}
+  	this.props.updateUser(username);
+  	this.props.addMessage(event.target.value, username);
+    event.target.value = "";
+  }
+}
   render() {
     return (
       <footer className='chatbar'>
-  	    <input className='chatbar-username' placeholder='Your Name (Optional)' defaultValue={this.props.name} />
-  	    <input className='chatbar-message' placeholder='Type a message and hit ENTER' />
+  	    <input id='username' className='chatbar-username' placeholder='Your Name (Optional)' defaultValue={this.props.name} />
+  	    <input id='message' className='chatbar-message' placeholder='Type a message and hit ENTER' onKeyPress={this.handleMessageKeyPress} />
 	  </footer>
     );
   }
+
 }
 export default ChatBar;
