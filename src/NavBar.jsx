1d753ee84beed6ecf9cdc0b4ef85f94fe4e72fc2
Karthikeyan Annadorai <karthik83@gmail.com>
2018-10-05 22:27:18 -0500

    First React Components Added

---
+++ src/NavBar.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class NavBar extends Component {
+ render() {
+   return (
+     <nav className='navbar'>
+	   <a href='/' className='navbar-brand'>Jatti</a>
+	 </nav>
+   );
+ }
+}
+export default NavBar;
