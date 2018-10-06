4a29d7d95c4d33e772315d08b5500ceadbc68cb1
Karthikeyan Annadorai <karthik83@gmail.com>
2018-10-06 10:46:17 -0500

    Initial state is set

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,24 @@ import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // this is the *only* time you should assign directly to state:
+    this.state = {
+	  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+	  messages: [
+	    {
+	      username: "Bob",
+	      content: "Has anyone seen my marbles?",
+	    },
+	    {
+	      username: "Anonymous",
+	      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+	    }
+	  ]
+	};
+  }
+
   render() {
     return (
 	  <div>
