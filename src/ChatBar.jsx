8535cdbabe319adc0850d02387849040e79f4966
Karthikeyan Annadorai <karthik83@gmail.com>
2018-10-06 16:00:11 -0500

    ChatBar code refactor. Added validation to not enter empty messages

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -8,16 +8,18 @@ handleMessageKeyPress = (event) => {
   	if (username === null || username === "") {
  	  username = "Anonymous";
   	}
-  	this.props.updateUser(username);
-  	this.props.addMessage(event.target.value, username);
+  	if (event.target.value !== null && event.target.value.trim() !== "") {
+  	  this.props.updateUser(username);
+  	  this.props.addMessage(event.target.value, username);
+  	}
     event.target.value = "";
   }
 }
   render() {
     return (
       <footer className='chatbar'>
-  	    <input id='username' className='chatbar-username' placeholder='Your Name (Optional)' defaultValue={this.props.name} />
-  	    <input id='message' className='chatbar-message' placeholder='Type a message and hit ENTER' onKeyPress={this.handleMessageKeyPress} />
+  	    <input id='username' maxLength='30' className='chatbar-username' placeholder='Your Name (Optional)' defaultValue={this.props.name} />
+  	    <input id='message' maxLength='280' className='chatbar-message' placeholder='Type a message and hit ENTER' onKeyPress={this.handleMessageKeyPress} />
 	  </footer>
     );
   }
